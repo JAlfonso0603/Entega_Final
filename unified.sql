8d1ee72c43e04450d706fcba7a55da676a06875d
Saalem45 <167729100+Saalem45@users.noreply.github.com>
2024-12-09 23:49:44 -0600

    Update unified.sql

--- unified.sql
+++ unified.sql
@@ -1,6 +1,6 @@
----------------------------------------------------------------------------------------------------------
+----------------------------------------------------------------------------------------------------------
 -- Procedimiento de: Jesús Alfonso Cuevas Ávila
----------------------------------------------------------------------------------------------------------
+----------------------------------------------------------------------------------------------------------
 
 CREATE OR REPLACE FUNCTION check_user(
 	_idUsuario INTEGER
@@ -196,6 +196,54 @@ LANGUAGE plpgsql;
 -- Ejemplo
 
 CALL realizar_prestamo(7, 1, 'manga b', 6);
+
+----------------------------------------------------------------------------------------------------------
+-- Fin del procedimiento
+----------------------------------------------------------------------------------------------------------
+----------------------------------------------------------------------------------------------------------
+-- Procedimiento de: Jesús Emmanuel Saldaña Álvarez
+----------------------------------------------------------------------------------------------------------
+
+CREATE OR REPLACE PROCEDURE obtener_autores_top_mangas()
+AS $$
+DECLARE
+    _resultado RECORD;
+    _existen BOOLEAN;
+BEGIN
+    -- Verificar si existen registros en las tablas involucradas
+    SELECT EXISTS (
+        SELECT 1
+        FROM DetallePrestamos DP
+        INNER JOIN Mangas M ON DP.idManga = M.idManga
+        INNER JOIN Autores A ON M.idAutor = A.idAutor
+    ) INTO _existen;
+
+    IF NOT _existen THEN
+        RAISE NOTICE 'No existen registros de autores o mangas relacionados con préstamos.';
+        RETURN;
+    END IF;
+
+    -- Consulta para obtener los IDs de autores de los mangas más prestados
+    FOR _resultado IN 
+        SELECT 
+            A.idAutor, 
+            M.nombreManga, 
+            SUM(DP.cantidad) AS totalPrestados
+        FROM DetallePrestamos DP
+        INNER JOIN Mangas M ON DP.idManga = M.idManga
+        INNER JOIN Autores A ON M.idAutor = A.idAutor
+        GROUP BY A.idAutor, M.nombreManga
+        ORDER BY totalPrestados DESC
+        LIMIT 3
+    LOOP
+        -- Mostrar los resultados
+        RAISE NOTICE 'ID Autor: %, Manga: %, Total Prestados: %', 
+            _resultado.idAutor, _resultado.nombreManga, _resultado.totalPrestados;
+    END LOOP;
+END;
+$$ LANGUAGE plpgsql;
+
+CALL obtener_autores_top_mangas();
 
 ----------------------------------------------------------------------------------------------------------
 -- Fin del procedimiento
